76fc21cb06d993c9aaa93637e794b12a5bd2ed9b
Van Vu Nguyen <nvvu6999@gmail.com>
2021-07-28 15:34:22 +0700

    Add UI and localization for profile related page
    UI:
      * Register.
      * View profile.
      * Update profile.

--- resources/lang/vi/validation.php
+++ resources/lang/vi/validation.php
@@ -3,7 +3,17 @@
 return [
     'required' => ':Attribute không được để trống.',
     'alpha_num' => ':Attribute chỉ bao gồm chữ cái và số.',
+    'unique' => ':Attribute đã tồn tại.',
+    'min' => [
+        'string' => ':Attribute tối thiểu :min ký tự.',
+    ],
+    'email' => ':Attribute không hợp lệ.',
+    'string' => ':Atribute phải là chuỗi ký tự.',
+    'confirmed' => ':Attribute xác thực không khớp.',
+
     'attributes' => [
         'password' => 'mật khẩu',
+        'first_name' => 'tên',
+        'last_name' => 'họ và tên đệm',
     ],
 ];
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,7 +6,12 @@
     </a>
     <section class="user-action">
         @guest
+            <a id="register-link" href="{{ route('register') }}">{{ __('labels.register') }}</a>
             <a id="login-link" href="{{ route('login') }}">{{ __('labels.login') }}</a>
         @endguest
+
+        @auth
+            <a id="logout-link" href="{{ route('logout') }}">{{ __('labels.logout') }}</a>
+        @endauth
     </section>
 </header>
